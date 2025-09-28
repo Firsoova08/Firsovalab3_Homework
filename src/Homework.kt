c69c46954385a6c5d63e18e09e4574bf6b5a33d0
Firsoova08 <foxalishca@mail.ru>
2025-09-28 13:14:56 +0300

    1

---
+++ src/Homework.kt
@@ -0,0 +1,19 @@
+import kotlin.random.Random
+fun main() {
+    //1
+//    val number = Random.nextInt(1, 50)
+//    while (true) {
+//        print("Введи число: ")
+//        val input = readln().toInt()
+//        when{
+//            input<number->println("больше")
+//            input>number->println("меньше")
+//            else->{
+//                println("ты угадал число")
+//                break
+//            }
+//        }
+//    }
+    //2
+
+}
